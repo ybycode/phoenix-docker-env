f52f11a55d58c838b936c9d0505a0e64189105d8
Yann B <ybycode@gmail.com>
2016-07-09 13:48:24 +0200

    Adapted scripts/_set-env.sh to check the compose override file existance and export its filename

--- scripts/_set-env.sh
+++ scripts/_set-env.sh
@@ -13,6 +13,10 @@ THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # cd to where the docker-compose.yml file is:
 cd $THIS_DIR/..
 
+################################################################################
+# The config/env.txt
+#
+
 ENV_FILENAME=$(readlink -f config/env.txt) # absolute filename because it can
                                            # be printed in errors.
 
@@ -25,6 +29,11 @@ if [[ ! -e "$ENV_FILENAME" ]]; then
 fi
 
 ENV=$(cat $ENV_FILENAME | tr -d '[[:space:]]')
+
+################################################################################
+# The config/env-${ENV}.sh
+#
+
 env_file=$(readlink -f config/env-${ENV}.sh)
 
 if [[ ! -e "$env_file" ]]; then
@@ -33,6 +42,19 @@ if [[ ! -e "$env_file" ]]; then
     echo Exiting
     exit 1
 fi
-
 source $env_file
+
+################################################################################
+# The docker-compose-${ENV}.yml
+#
+
+DOCKER_COMPOSE_FILE=docker-compose-${ENV}.yml
+
+if [[ ! -e "$DOCKER_COMPOSE_FILE" ]]; then
+    echo Error: the file $DOCKER_COMPOSE_FILE doesn\'t exist.
+    echo Exiting
+    exit 1
+fi
+
+export DOCKER_COMPOSE_FILE
 
